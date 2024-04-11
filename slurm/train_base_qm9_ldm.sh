3deb09f9fe05421a30f1dcad2da6a9f921271cfd
PIEthonista <gohyixian456@gmail.com>
2024-04-12 00:54:38 +0800

    dicc

--- slurm/train_base_qm9_ldm.sh
+++ slurm/train_base_qm9_ldm.sh
@@ -5,18 +5,18 @@
 #SBATCH --nodes=1
 #SBATCH --mem=100G
 #SBATCH --gpus=1
-#SBATCH --job-name=train_base_qm9
-#SBATCH --output=slurm_out/train_base_qm9.out
-#SBATCH --error=slurm_err/train_base_qm9.err
+#SBATCH --job-name=train_base_qm9_ldm
+#SBATCH --output=slurm_out/train_base_qm9_ldm.out
+#SBATCH --error=slurm_err/train_base_qm9_ldm.err
 #SBATCH --qos=long
 #SBATCH --hint=multithread
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=gohyixian456@gmail.com
 
-# 80460
+# 80454
 # module los cuda/12.4
 module load cuda/12.1
 module load miniconda/24.1.2
 conda activate geoldm
 python check_gpu.py
-python main_qm9.py --config_file custom_config/base_qm9_config.yaml
+python main_qm9.py --config_file custom_config/base_qm9_ldm_config.yaml
--- slurm/train_base_qmy_vae.sh
+++ slurm/train_base_qmy_vae.sh
@@ -1,22 +1,22 @@
 #! /bin/bash -l
 
-#SBATCH --partition=gpu-a100-mig
+#SBATCH --partition=gpu-a100
 #SBATCH --ntasks=4
 #SBATCH --nodes=1
 #SBATCH --mem=100G
 #SBATCH --gpus=1
-#SBATCH --job-name=train_base_qm9
-#SBATCH --output=slurm_out/train_base_qm9.out
-#SBATCH --error=slurm_err/train_base_qm9.err
+#SBATCH --job-name=train_base_qm9_vae
+#SBATCH --output=slurm_out/train_base_qm9_vae.out
+#SBATCH --error=slurm_err/train_base_qm9_vae.err
 #SBATCH --qos=long
 #SBATCH --hint=multithread
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=gohyixian456@gmail.com
 
-# 80460
+# 80461
 # module los cuda/12.4
 module load cuda/12.1
 module load miniconda/24.1.2
 conda activate geoldm
 python check_gpu.py
-python main_qm9.py --config_file custom_config/base_qm9_config.yaml
+python main_qm9.py --config_file custom_config/base_qm9_vae_config.yaml
