99d232b165e964d7b9d974ad7e1bb6f8fca455df
PIEthonista <gohyixian456@gmail.com>
2024-04-13 17:17:07 +0800

    dicc

---
+++ slurm/v100_train_base_qmy_1st_vae.sh
@@ -0,0 +1,22 @@
+#! /bin/bash -l
+
+#SBATCH --partition=gpu-v100s
+#SBATCH --ntasks=4
+#SBATCH --nodes=1
+#SBATCH --mem=100G
+#SBATCH --gpus=1
+#SBATCH --job-name=train_base_qm9_vae
+#SBATCH --output=slurm_out/train_base_qm9_vae.out
+#SBATCH --error=slurm_err/train_base_qm9_vae.err
+#SBATCH --qos=long
+#SBATCH --hint=multithread
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=gohyixian456@gmail.com
+
+# 80550
+module load cuda/cuda-11.8
+module load miniconda/miniconda3
+conda activate geoldm
+cd /home/user/yixian.goh/geoldm-edit
+python check_gpu.py
+python main_qm9.py --config_file custom_config/base_qm9_vae_config.yaml
