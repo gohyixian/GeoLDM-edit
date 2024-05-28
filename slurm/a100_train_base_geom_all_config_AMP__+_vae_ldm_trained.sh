5f29451ad4a781d37b12a91e5b841cf5162dfc20
PIEthonista <gohyixian456@gmail.com>
2024-05-28 17:19:43 +0800

    dicc'

--- slurm/a100_train_base_geom_all_config_AMP__+_vae_ldm_trained.sh
+++ slurm/a100_train_base_geom_all_config_AMP__+_vae_ldm_trained.sh
@@ -5,19 +5,19 @@
 #SBATCH --nodes=1
 #SBATCH --mem=100G
 #SBATCH --gpus=1
-#SBATCH --job-name=train_base_geom_vae_config___20240528
-#SBATCH --output=slurm_out/train_base_geom_vae_config___20240528.out
-#SBATCH --error=slurm_err/train_base_geom_vae_config___20240528.err
+#SBATCH --job-name=train_base_geom_all_config_AMP__+_vae_ldm_trained_20240528
+#SBATCH --output=slurm_out/train_base_geom_all_config_AMP__+_vae_ldm_trained_20240528.out
+#SBATCH --error=slurm_err/train_base_geom_all_config_AMP__+_vae_ldm_trained_20240528.err
 #SBATCH --qos=long
 #SBATCH --hint=multithread
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=gohyixian456@gmail.com
 
-# 82618
+# 88190
 module load cuda/12.1       # gpu-a100
 module load miniconda/24.1.2
 conda activate geoldm-a100
 
 cd /home/user/yixian.goh/geoldm-edit
 python check_gpu.py
-python main_geom_drugs.py --config_file custom_config/base_geom_vae_config__.yaml
+python main_geom_drugs.py --config_file custom_config/geom/base_geom_all_config_AMP__+_vae_ldm_trained.yaml
