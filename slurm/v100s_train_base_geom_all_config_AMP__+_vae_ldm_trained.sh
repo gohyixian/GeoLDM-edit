3aec63d08f546bdb7fa3e7bca9c83ac99edd446b
PIEthonista <gohyixian456@gmail.com>
2024-06-01 18:56:55 +0800

    dicc

--- slurm/v100s_train_base_geom_all_config_AMP__+_vae_ldm_trained.sh
+++ slurm/v100s_train_base_geom_all_config_AMP__+_vae_ldm_trained.sh
@@ -1,10 +1,10 @@
 #! /bin/bash -l
 
-#SBATCH --partition=gpu-a100
-#SBATCH --ntasks=4
+#SBATCH --partition=gpu-v100s
+#SBATCH --ntasks=8
 #SBATCH --nodes=1
 #SBATCH --mem=100G
-#SBATCH --gpus=1
+#SBATCH --gpus=2
 #SBATCH --job-name=train_base_geom_all_config_AMP__+_vae_ldm_trained_20240601
 #SBATCH --output=slurm_out/train_base_geom_all_config_AMP__+_vae_ldm_trained_20240601.out
 #SBATCH --error=slurm_err/train_base_geom_all_config_AMP__+_vae_ldm_trained_20240601.err
@@ -13,10 +13,16 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=gohyixian456@gmail.com
 
-# 88817
-module load cuda/12.1       # gpu-a100
-module load miniconda/24.1.2
-conda activate geoldm-a100
+# 88818
+# module load cuda/12.1       # gpu-a100
+# module load miniconda/24.1.2
+# conda activate geoldm-a100
+
+
+module load cuda/cuda-11.8  # gpu-v100s
+module load miniconda/miniconda3
+conda activate geoldm
+
 
 cd /home/user/yixian.goh/geoldm-edit
 python check_gpu.py
