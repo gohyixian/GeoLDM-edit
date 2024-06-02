4b6aaa566c4b4d8ee7262924770ba23c439452a8
PIEthonista <gohyixian456@gmail.com>
2024-06-02 16:45:21 +0800

    dicc

--- slurm/v100s_train_base_geom_all_config_AMP__+_vae_ldm_trained.sh
+++ slurm/v100s_train_base_geom_all_config_AMP__+_vae_ldm_trained.sh
@@ -5,15 +5,15 @@
 #SBATCH --nodes=1
 #SBATCH --mem=100G
 #SBATCH --gpus=2
-#SBATCH --job-name=train_base_geom_all_config_AMP__+_vae_ldm_trained_20240601
-#SBATCH --output=slurm_out/train_base_geom_all_config_AMP__+_vae_ldm_trained_20240601.out
-#SBATCH --error=slurm_err/train_base_geom_all_config_AMP__+_vae_ldm_trained_20240601.err
+#SBATCH --job-name=train_base_geom_all_config_AMP__+_vae_ldm_trained_20240602
+#SBATCH --output=slurm_out/train_base_geom_all_config_AMP__+_vae_ldm_trained_20240602.out
+#SBATCH --error=slurm_err/train_base_geom_all_config_AMP__+_vae_ldm_trained_20240602.err
 #SBATCH --qos=long
 #SBATCH --hint=multithread
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=gohyixian456@gmail.com
 
-# 88836
+# 88873
 # module load cuda/12.1       # gpu-a100
 # module load miniconda/24.1.2
 # conda activate geoldm-a100
