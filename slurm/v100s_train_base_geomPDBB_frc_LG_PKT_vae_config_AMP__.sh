8cb77dee01b2e4a99dfdbdf1855df721a9a5234a
PIEthonista <gohyixian456@gmail.com>
2024-06-03 18:48:20 +0800

    dicc

--- slurm/v100s_train_base_geomPDBB_frc_LG_PKT_vae_config_AMP__.sh
+++ slurm/v100s_train_base_geomPDBB_frc_LG_PKT_vae_config_AMP__.sh
@@ -5,15 +5,15 @@
 #SBATCH --nodes=1
 #SBATCH --mem=100G
 #SBATCH --gpus=2
-#SBATCH --job-name=train_base_geom_all_config_AMP__+_vae_ldm_trained_20240603_no_vis_test
-#SBATCH --output=slurm_out/train_base_geom_all_config_AMP__+_vae_ldm_trained_20240603_no_vis_test.out
-#SBATCH --error=slurm_err/train_base_geom_all_config_AMP__+_vae_ldm_trained_20240603_no_vis_test.err
+#SBATCH --job-name=train_base_geomPDBB_frc_LG_PKT_vae_config__noAMP_bs2_nf64__20240603
+#SBATCH --output=slurm_out/train_base_geomPDBB_frc_LG_PKT_vae_config__noAMP_bs2_nf64__20240603.out
+#SBATCH --error=slurm_err/train_base_geomPDBB_frc_LG_PKT_vae_config__noAMP_bs2_nf64__20240603.err
 #SBATCH --qos=long
 #SBATCH --hint=multithread
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=gohyixian456@gmail.com
 
-# 89263
+# 89262
 # module load cuda/12.1       # gpu-a100
 # module load miniconda/24.1.2
 # conda activate geoldm-a100
@@ -23,7 +23,6 @@ module load cuda/cuda-11.8  # gpu-v100s
 module load miniconda/miniconda3
 conda activate geoldm
 
-
 cd /home/user/yixian.goh/geoldm-edit
 python check_gpu.py
-python main_geom_drugs.py --config_file custom_config/geom/base_geom_all_config_AMP__+_vae_ldm_trained.yaml
+python main_geom_drugs.py --config_file custom_config/geomPDBB/base_geomPDBB_frc_LG_PKT_vae_config_AMP__.yaml
