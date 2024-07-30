0e459237aa9a1d1366b5d30616208c7a94448f15
PIEthonista <gohyixian456@gmail.com>
2024-07-30 22:10:42 +0800

    dicc slurm update

--- data/d_20240623_CrossDocked_LG_PKT/README.txt
+++ data/d_20240623_CrossDocked_LG_PKT/README.txt
@@ -4,7 +4,7 @@ gdown --fuzzy https://drive.google.com/file/d/1GzcG6BlZVu4SKjvPFkneKMPQDI_HKHxL/
 
 gdown --fuzzy https://drive.google.com/file/d/1qH66fPXp2M8-ocn7s0DgVj9b5zUg0Mja/view?usp=sharing
 
-gdown --fuzzy https://drive.google.com/file/d/1GzcG6BlZVu4SKjvPFkneKMPQDI_HKHxL/view?usp=sharing
+gdown --fuzzy https://drive.google.com/file/d/1OVCrIRiUL0Ri76hdWnIUMgWJu3JYVcQA/view?usp=sharing
 
 cd path_to_destination_folder
 unzip zipname
---
+++ slurm/AMP_test/20240623__10A/titan_AMP_TEST__01_VAE_vaenorm_False__float32__latent2_nf32_epoch10_bs4_lr1e-4__20240623__10A.sh
@@ -0,0 +1,29 @@
+#! /bin/bash -l
+
+#SBATCH --partition=gpu-titan
+#SBATCH --ntasks=16
+#SBATCH --nodes=1
+#SBATCH --mem=100G
+#SBATCH --gpus=2
+#SBATCH --job-name=AMP_TEST__01_VAE_vaenorm_False__float32__latent2_nf32_epoch10_bs4_lr1e-4__20240623__10A
+#SBATCH --output=slurm_out/AMP_test/20240623__10A/AMP_TEST__01_VAE_vaenorm_False__float32__latent2_nf32_epoch10_bs4_lr1e-4__20240623__10A.out
+#SBATCH --error=slurm_err/AMP_test/20240623__10A/AMP_TEST__01_VAE_vaenorm_False__float32__latent2_nf32_epoch10_bs4_lr1e-4__20240623__10A.err
+#SBATCH --qos=long
+#SBATCH --hint=multithread
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=gohyixian456@gmail.com
+
+# 101225
+# module load cuda/12.1       # gpu-a100
+# module load miniconda/24.1.2
+# conda activate geoldm-a100
+
+
+module load cuda/cuda-11.8  # gpu-v100s or others
+module load miniconda/24.1.2
+conda activate geoldm
+
+
+cd /home/user/yixian.goh/geoldm-edit
+python check_gpu.py
+python main_geom_drugs.py --config_file custom_config/CrossDocked/20240623__10A/AMP/AMP_TEST__01_VAE_vaenorm_False__float32__latent2_nf32_epoch10_bs4_lr1e-4__20240623__10A.yaml
