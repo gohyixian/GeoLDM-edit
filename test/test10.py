4ae6209cafa446c1590c72bbc018217844dc5be1
yxxx <gohyixian456@gmail.com>
2024-08-26 14:49:01 +0800

    config update

--- test/test10.py
+++ test/test10.py
@@ -1,11 +1,11 @@
 low = -4000
 high = 4000
-range_min = 0
+range_min = -1
 range_max = 1
 
 m = (range_max - range_min) / (high - low)
 print(m)
 
-c = abs(m*low)
+c = range_min - (m * low)
 print(c)
 
