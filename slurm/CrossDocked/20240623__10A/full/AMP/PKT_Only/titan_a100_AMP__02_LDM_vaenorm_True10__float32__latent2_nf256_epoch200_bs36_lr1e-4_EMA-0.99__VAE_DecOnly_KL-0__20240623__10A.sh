830c306baecd7737422197b9caed715fe2a11527
PIEthonista <gohyixian456@gmail.com>
2024-10-16 16:39:25 +0800

    dicc

---
+++ slurm/CrossDocked/20240623__10A/full/AMP/PKT_Only/titan_a100_AMP__02_LDM_vaenorm_True10__float32__latent2_nf256_epoch200_bs36_lr1e-4_EMA-0.99__VAE_DecOnly_KL-0__20240623__10A.sh
@@ -0,0 +1,33 @@
+#! /bin/bash -l
+
+#SBATCH --partition=gpu-a100
+#SBATCH --ntasks=30
+#SBATCH --nodes=1
+#SBATCH --mem=60G
+#SBATCH --gpus=1
+#SBATCH --job-name=AMP__02_LDM_vaenorm_True10__float32__latent2_nf256_epoch200_bs36_lr1e-4_EMA-0.99__VAE_DecOnly_KL-0__20240623__10A
+#SBATCH --output=slurm_out/AMP__02_LDM_vaenorm_True10__float32__latent2_nf256_epoch200_bs36_lr1e-4_EMA-0.99__VAE_DecOnly_KL-0__20240623__10A.out
+#SBATCH --error=slurm_err/AMP__02_LDM_vaenorm_True10__float32__latent2_nf256_epoch200_bs36_lr1e-4_EMA-0.99__VAE_DecOnly_KL-0__20240623__10A.err
+#SBATCH --qos=long
+#SBATCH --hint=multithread
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=gohyixian456@gmail.com
+
+# 116483
+module load cuda/12.1       # gpu-a100
+module load miniconda/24.1.2
+conda activate geoldm-a100
+
+
+# module load cuda/cuda-11.8  # gpu-v100s
+# module load miniconda/miniconda3
+# conda activate geoldm
+
+
+# module load cuda/cuda-11.8  # gpu-titan
+# module load miniconda/24.1.2
+# conda activate geoldm
+
+cd /home/user/yixian.goh/geoldm-edit
+python check_gpu.py
+python main_geom_drugs.py --config_file custom_config/CrossDocked/20240623__10A/full/AMP/ldm/best/AMP__02_LDM_vaenorm_True10__float32__latent2_nf256_epoch200_bs36_lr1e-4_EMA-0.99__VAE_DecOnly_KL-0__20240623__10A.yaml
