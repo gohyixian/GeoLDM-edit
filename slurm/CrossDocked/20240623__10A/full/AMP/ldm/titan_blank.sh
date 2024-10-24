30f5948d23b80efa2ad46dbb5cf08d6672974645
PIEthonista <gohyixian456@gmail.com>
2024-10-24 15:57:17 +0800

    dicc

--- slurm/CrossDocked/20240623__10A/full/AMP/ldm/titan_blank.sh
+++ slurm/CrossDocked/20240623__10A/full/AMP/ldm/titan_blank.sh
@@ -5,15 +5,15 @@
 #SBATCH --nodes=1
 #SBATCH --mem=60G
 #SBATCH --gpus=1
-#SBATCH --job-name=AMP__02_LDM_vaenorm_True10__float32__latent8_nf128_epoch1000_bs80_lr1e-4_EMA-0.75__20240623__10A
-#SBATCH --output=slurm_out/AMP__02_LDM_vaenorm_True10__float32__latent8_nf128_epoch1000_bs80_lr1e-4_EMA-0.75__20240623__10A.out
-#SBATCH --error=slurm_err/AMP__02_LDM_vaenorm_True10__float32__latent8_nf128_epoch1000_bs80_lr1e-4_EMA-0.75__20240623__10A.err
+#SBATCH --job-name=AMP__01_VAE_vaenorm_True__bfloat16__latent4_nf64_epoch100_bs36_lr1e-5__20240623__10A__PKT_Only
+#SBATCH --output=slurm_out/AMP__01_VAE_vaenorm_True__bfloat16__latent4_nf64_epoch100_bs36_lr1e-5__20240623__10A__PKT_Only.out
+#SBATCH --error=slurm_err/AMP__01_VAE_vaenorm_True__bfloat16__latent4_nf64_epoch100_bs36_lr1e-5__20240623__10A__PKT_Only.err
 #SBATCH --qos=long
 #SBATCH --hint=multithread
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=gohyixian456@gmail.com
 
-# 115668
+# 103805
 # module load cuda/12.1       # gpu-a100
 # module load miniconda/24.1.2
 # conda activate geoldm-a100
@@ -30,4 +30,4 @@ conda activate geoldm
 
 cd /home/user/yixian.goh/geoldm-edit
 python check_gpu.py
-python main_geom_drugs.py --config_file custom_config/CrossDocked/20240623__10A/full/AMP/ldm/AMP__02_LDM_vaenorm_True10__float32__latent8_nf128_epoch1000_bs80_lr1e-4_EMA-0.75__20240623__10A.yaml
+python main_geom_drugs.py --config_file custom_config/CrossDocked/20240623__10A/full/AMP/PKT_Only/AMP__01_VAE_vaenorm_True__bfloat16__latent4_nf64_epoch100_bs36_lr1e-5__20240623__10A__PKT_Only.yaml
