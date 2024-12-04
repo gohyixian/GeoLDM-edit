1f982405041a8ee58587b90d38ff29af7a07033b
PIEthonista <gohyixian456@gmail.com>
2024-12-04 12:25:16 +0800

    dicc

--- slurm/CrossDocked/20240623__10A/full/AMP/controlnet/a100_03_latent2_nf256_ds1k_fusBSum_CA__epoch1k_bs64_lr1e-4_NoEMA__20241115__10A.sh
+++ slurm/CrossDocked/20240623__10A/full/AMP/controlnet/a100_03_latent2_nf256_ds1k_fusBSum_CA__epoch1k_bs64_lr1e-4_NoEMA__20241115__10A.sh
@@ -5,15 +5,15 @@
 #SBATCH --nodes=1
 #SBATCH --mem=100G
 #SBATCH --gpus=2
-#SBATCH --job-name=03_latent2_nf256_ds1k_fusBSum__epoch1k_bs8_lr1e-4_NoEMA__20241115__10A
-#SBATCH --output=slurm_out/03_latent2_nf256_ds1k_fusBSum__epoch1k_bs8_lr1e-4_NoEMA__20241115__10A.out
-#SBATCH --error=slurm_err/03_latent2_nf256_ds1k_fusBSum__epoch1k_bs8_lr1e-4_NoEMA__20241115__10A.err
+#SBATCH --job-name=03_latent2_nf256_ds1k_fusBSum_CA__epoch1k_bs64_lr1e-4_NoEMA__20241115__10A
+#SBATCH --output=slurm_out/03_latent2_nf256_ds1k_fusBSum_CA__epoch1k_bs64_lr1e-4_NoEMA__20241115__10A.out
+#SBATCH --error=slurm_err/03_latent2_nf256_ds1k_fusBSum_CA__epoch1k_bs64_lr1e-4_NoEMA__20241115__10A.err
 #SBATCH --qos=long
 #SBATCH --hint=multithread
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=gohyixian456@gmail.com
 
-# 124413
+# 124461
 module load cuda/12.1       # gpu-a100
 module load miniconda/24.1.2
 conda activate geoldm-a100
@@ -27,4 +27,4 @@ conda activate geoldm-a100
 cd /home/user/yixian.goh/geoldm-edit
 chmod +x analysis/qvina/qvina2.1
 python check_gpu.py
-python main_geom_drugs_control.py --config_file custom_config/CrossDocked/20240623__10A/full/AMP/controlnet/03_latent2_nf256_ds1k_fusBSum__epoch1k_bs8_lr1e-4_NoEMA__20241115__10A.yaml
+python main_geom_drugs_control.py --config_file custom_config/CrossDocked/20240623__10A/full/AMP/controlnet/03_latent2_nf256_ds1k_fusBSum_CA__epoch1k_bs64_lr1e-4_NoEMA__20241115__10A.yaml
