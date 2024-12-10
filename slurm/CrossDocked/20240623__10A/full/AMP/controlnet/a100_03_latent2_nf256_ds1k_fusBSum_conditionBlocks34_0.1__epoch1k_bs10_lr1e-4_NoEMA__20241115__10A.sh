b826961a92b743748eb86de9c8aa3954909a87bf
PIEthonista <gohyixian456@gmail.com>
2024-12-10 12:20:26 +0800

    dicc

--- slurm/CrossDocked/20240623__10A/full/AMP/controlnet/a100_03_latent2_nf256_ds1k_fusBSum_conditionBlocks34_0.1__epoch1k_bs10_lr1e-4_NoEMA__20241115__10A.sh
+++ slurm/CrossDocked/20240623__10A/full/AMP/controlnet/a100_03_latent2_nf256_ds1k_fusBSum_conditionBlocks34_0.1__epoch1k_bs10_lr1e-4_NoEMA__20241115__10A.sh
@@ -13,7 +13,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=gohyixian456@gmail.com
 
-# 124582
+# 125108
 module load cuda/12.1       # gpu-a100
 module load miniconda/24.1.2
 conda activate geoldm-a100
