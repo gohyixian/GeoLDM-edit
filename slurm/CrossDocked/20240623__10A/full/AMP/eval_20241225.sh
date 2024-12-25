ee297b62334de4597ca4d7bf487eb495a005c909
PIEthonista <gohyixian456@gmail.com>
2024-12-25 23:51:06 +0800

    dicc

--- slurm/CrossDocked/20240623__10A/full/AMP/eval_20241225.sh
+++ slurm/CrossDocked/20240623__10A/full/AMP/eval_20241225.sh
@@ -13,7 +13,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=gohyixian456@gmail.com
 
-# 129722
+# 129737
 module load cuda/12.1       # gpu-a100
 module load miniconda/24.1.2
 conda activate geoldm-a100
@@ -30,16 +30,16 @@ python check_gpu.py
 
 
 # CA conditionAll 0.1 0.5 0.9
-python eval_analyze_controlnet.py --model_path outputs_selected/controlnet/03_latent2_nf256_ds1k_fusBSum_CA_conditionAll_0.1__epoch1k_bs60_lr1e-4_NoEMA__20241203__10A_3x_resume --load_last --pocket_pdb_dir data/d_20241203_CrossDocked_LG_PKT_MMseq2_split_CA_only/test_val_paired_files/val_pocket --num_samples_per_pocket 100 --delta_num_atoms 5 --batch_size 128 --seed 42 --cleanup_files
-python eval_analyze_controlnet.py --model_path outputs_selected/controlnet/03_latent2_nf256_ds1k_fusBSum_CA_conditionAll_0.5__epoch1k_bs60_lr1e-4_NoEMA__20241203__10A_3x_resume --load_last --pocket_pdb_dir data/d_20241203_CrossDocked_LG_PKT_MMseq2_split_CA_only/test_val_paired_files/val_pocket --num_samples_per_pocket 100 --delta_num_atoms 5 --batch_size 128 --seed 42 --cleanup_files
-python eval_analyze_controlnet.py --model_path outputs_selected/controlnet/03_latent2_nf256_ds1k_fusBSum_CA_conditionAll_0.9__epoch1k_bs60_lr1e-4_NoEMA__20241203__10A_1x_resume --load_last --pocket_pdb_dir data/d_20241203_CrossDocked_LG_PKT_MMseq2_split_CA_only/test_val_paired_files/val_pocket --num_samples_per_pocket 100 --delta_num_atoms 5 --batch_size 128 --seed 42 --cleanup_files
+python eval_analyze_controlnet.py --model_path outputs_selected/controlnet/03_latent2_nf256_ds1k_fusBSum_CA_conditionAll_0.1__epoch1k_bs60_lr1e-4_NoEMA__20241203__10A_3x_resume --load_last --pocket_pdb_dir data/d_20241203_CrossDocked_LG_PKT_MMseq2_split_CA_only/test_val_paired_files/val_pocket --num_samples_per_pocket 100 --delta_num_atoms 5 --batch_size 512 --seed 42 --cleanup_files
+python eval_analyze_controlnet.py --model_path outputs_selected/controlnet/03_latent2_nf256_ds1k_fusBSum_CA_conditionAll_0.5__epoch1k_bs60_lr1e-4_NoEMA__20241203__10A_3x_resume --load_last --pocket_pdb_dir data/d_20241203_CrossDocked_LG_PKT_MMseq2_split_CA_only/test_val_paired_files/val_pocket --num_samples_per_pocket 100 --delta_num_atoms 5 --batch_size 512 --seed 42 --cleanup_files
+python eval_analyze_controlnet.py --model_path outputs_selected/controlnet/03_latent2_nf256_ds1k_fusBSum_CA_conditionAll_0.9__epoch1k_bs60_lr1e-4_NoEMA__20241203__10A_1x_resume --load_last --pocket_pdb_dir data/d_20241203_CrossDocked_LG_PKT_MMseq2_split_CA_only/test_val_paired_files/val_pocket --num_samples_per_pocket 100 --delta_num_atoms 5 --batch_size 512 --seed 42 --cleanup_files
 
 # CA conditionBlocks34 0.1 0.5
-python eval_analyze_controlnet.py --model_path outputs_selected/controlnet/03_latent2_nf256_ds1k_fusBSum_CA_conditionBlocks34_0.1__epoch1k_bs60_lr1e-4_NoEMA__20241203__10A_3x_resume --load_last --pocket_pdb_dir data/d_20241203_CrossDocked_LG_PKT_MMseq2_split_CA_only/test_val_paired_files/val_pocket --num_samples_per_pocket 100 --delta_num_atoms 5 --batch_size 128 --seed 42 --cleanup_files
-python eval_analyze_controlnet.py --model_path outputs_selected/controlnet/03_latent2_nf256_ds1k_fusBSum_CA_conditionBlocks34_0.5__epoch1k_bs60_lr1e-4_NoEMA__20241203__10A_3x_resume --load_last --pocket_pdb_dir data/d_20241203_CrossDocked_LG_PKT_MMseq2_split_CA_only/test_val_paired_files/val_pocket --num_samples_per_pocket 100 --delta_num_atoms 5 --batch_size 128 --seed 42 --cleanup_files
+python eval_analyze_controlnet.py --model_path outputs_selected/controlnet/03_latent2_nf256_ds1k_fusBSum_CA_conditionBlocks34_0.1__epoch1k_bs60_lr1e-4_NoEMA__20241203__10A_3x_resume --load_last --pocket_pdb_dir data/d_20241203_CrossDocked_LG_PKT_MMseq2_split_CA_only/test_val_paired_files/val_pocket --num_samples_per_pocket 100 --delta_num_atoms 5 --batch_size 512 --seed 42 --cleanup_files
+python eval_analyze_controlnet.py --model_path outputs_selected/controlnet/03_latent2_nf256_ds1k_fusBSum_CA_conditionBlocks34_0.5__epoch1k_bs60_lr1e-4_NoEMA__20241203__10A_3x_resume --load_last --pocket_pdb_dir data/d_20241203_CrossDocked_LG_PKT_MMseq2_split_CA_only/test_val_paired_files/val_pocket --num_samples_per_pocket 100 --delta_num_atoms 5 --batch_size 512 --seed 42 --cleanup_files
 
 # CA fusReplace
-python eval_analyze_controlnet.py --model_path outputs_selected/controlnet/03_latent2_nf256_ds1k_fusReplace_CA__epoch1k_bs60_lr1e-4_NoEMA__20241203__10A --load_last --pocket_pdb_dir data/d_20241203_CrossDocked_LG_PKT_MMseq2_split_CA_only/test_val_paired_files/val_pocket --num_samples_per_pocket 100 --delta_num_atoms 5 --batch_size 128 --seed 42 --cleanup_files
+python eval_analyze_controlnet.py --model_path outputs_selected/controlnet/03_latent2_nf256_ds1k_fusReplace_CA__epoch1k_bs60_lr1e-4_NoEMA__20241203__10A --load_last --pocket_pdb_dir data/d_20241203_CrossDocked_LG_PKT_MMseq2_split_CA_only/test_val_paired_files/val_pocket --num_samples_per_pocket 100 --delta_num_atoms 5 --batch_size 512 --seed 42 --cleanup_files
 
 # ALL conditionBlocks34 0.1
 python eval_analyze_controlnet.py --model_path outputs_selected/controlnet/03_latent2_nf256_ds1k_fusBSum_conditionBlocks34_0.1__epoch1k_bs10_lr1e-4_NoEMA__20241115__10A_2x_resume --load_last --pocket_pdb_dir data/d_20241115_CrossDocked_LG_PKT_MMseq2_split/test_val_paired_files/val_pocket --num_samples_per_pocket 100 --delta_num_atoms 5 --batch_size 5 --seed 42 --cleanup_files
